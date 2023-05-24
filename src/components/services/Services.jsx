3d4f741197e0433c4268a4616a7eb1b03eaf4780
phuc1997 <phuc.che@cyberlogitec.com>
2023-05-24 11:58:49 +0700

    Update

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,49 +1,72 @@
-import React from 'react'
-import './services.css'
-import {BiCheck} from 'react-icons/bi'
+import React from "react";
+import "./services.css";
+import { BiCheck } from "react-icons/bi";
 
 const Services = () => {
   return (
-    <section id='services'>
+    <section id="services">
       <h5>What I Offer</h5>
       <h2>Services</h2>
 
       <div className="container services__container">
-        <article className='service'>
+        <article className="service">
           <div className="service__head">
-          <h3>Web development</h3>
+            <h2>
+              <b>Web development</b>
+            </h2>
           </div>
-          
-          <ul className='service__list'>
+
+          <ul className="service__list">
             <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Front-end development: A full-stack developer can create user-friendly, interactive, and responsive front-end designs using languages like HTML, CSS, JavaScript, ...</p>
+              <BiCheck className="service__list-icon" />
+              <p>
+                <b>Front-end development:</b> create user-friendly, responsive
+                front-end designs using HTML, CSS, JavaScript, ...
+              </p>
             </li>
             <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Back-end development: A full-stack developer can build robust and scalable back-end systems using technologies like Java and Node.js.</p>
+              <BiCheck className="service__list-icon" />
+              <p>
+                <b>Back-end development:</b> build scalable back-end systems
+                using Java and Node.js.
+              </p>
             </li>
             <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Database management: A full-stack developer can manage databases and perform tasks like database design, optimization, and maintenance.</p>
+              <BiCheck className="service__list-icon" />
+              <p>
+                <b>Database management:</b> A full-stack developer can manage
+                databases and perform tasks like database design, optimization,
+                and maintenance.
+              </p>
             </li>
             <li>
-              <BiCheck className='service__list-icon'/>
-              <p>API development: A full-stack developer can create RESTful APIs and integrate them with front-end and back-end systems.</p>
+              <BiCheck className="service__list-icon" />
+              <p>
+                <b>API development:</b> create RESTful APIs and integrate them
+                with front-end and back-end systems.
+              </p>
             </li>
             <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Deployment and testing: A full-stack developer can ensure the smooth deployment of web applications and conduct testing to ensure they work as expected.</p>
+              <BiCheck className="service__list-icon" />
+              <p>
+                <b>Deployment and testing:</b> ensure the smooth deployment of
+                web applications and conduct testing to ensure it work as
+                expected.
+              </p>
             </li>
             <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Continuous improvement: A full-stack developer can continuously improve the web application by fixing bugs, adding new features, and optimizing performance.</p>
+              <BiCheck className="service__list-icon" />
+              <p>
+                <b>Continuous improvement:</b> continuously improve the web
+                application by fixing bugs, adding new features, and optimizing
+                performance.
+              </p>
             </li>
           </ul>
         </article>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
